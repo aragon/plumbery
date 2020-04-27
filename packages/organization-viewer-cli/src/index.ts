a0cda67b42326abed6c2708bc9e571e5a9ec297e
Pierre Bertet <hello@pierre.world>
2020-04-27 16:19:32 +0100

    Formatting

--- packages/organization-viewer-cli/src/index.ts
+++ packages/organization-viewer-cli/src/index.ts
@@ -2,10 +2,12 @@ import { aragonConnect, ConnectorJson } from 'plumbery-core'
 import data from './org-data.json'
 
 async function main() {
+
   const connection = aragonConnect({
     connector: ['json', { data }],
     signer: {},
   })
+
   const org = connection.organization('governance.aragonproject.eth')
 
   console.log('Permissions:')
