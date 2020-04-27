46f51b0f14b713c26c2c3be0dc16dcba5505a0a1
Pierre Bertet <hello@pierre.world>
2020-04-27 17:19:54 +0100

    Update CLI demo

--- packages/organization-viewer-cli/src/index.ts
+++ packages/organization-viewer-cli/src/index.ts
@@ -1,17 +1,34 @@
-import { aragonConnect, ConnectorJson } from 'plumbery-core'
+import { aragonConnect } from 'plumbery-core'
 import data from './org-data.json'
 
 async function main() {
-
+  // Initiate the connection
   const connection = aragonConnect({
     connector: ['json', { data }],
     signer: {},
   })
 
+  // Get an Organization instance
   const org = connection.organization('governance.aragonproject.eth')
 
-  console.log('Permissions:')
-  console.log(await org.permissions())
+  // Get the permissions set on the organization
+  const permissions = await org.permissions()
+
+  logPermissions(permissions)
+}
+
+function logPermissions(permissions) {
+  console.log('')
+  console.log('Permissions')
+  console.log('===========')
+
+  for (const [app, roles] of permissions) {
+    console.log('')
+    console.log(`${app}:`)
+    for (const [role, entities] of roles) {
+      console.log(`  ${role}:`, entities.join(', '))
+    }
+  }
 }
 
 main()
