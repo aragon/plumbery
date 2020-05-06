b9b61cde2aa51847e8b5031768fe6243d31bb99a
ajs <palebluedot@gmail.com>
2020-05-05 23:30:33 -0300

    Added a script for first time build

---
+++ scripts/first-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# Typescript is failing to apply project references on the very first time that the project is built.
+# See https://www.typescriptlang.org/docs/handbook/project-references.html#caveats-for-project-references
+# While a better solution is found, the workaround is to build the project twice on the first build.
+
+yarn clean
+
+echo Clean install...
+yarn build &>/dev/null
+yarn build
