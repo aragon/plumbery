a1c5a26f39b7a2795adad6f81a07c2de63b50d3d
ajs <palebluedot@gmail.com>
2020-05-22 14:50:28 -0300

    Dynamically hooking MiniMeToken data sources

--- packages/token-manager-subgraph/src/TokenManager.ts
+++ packages/token-manager-subgraph/src/TokenManager.ts
@@ -2,6 +2,18 @@ import { NewVesting as NewVestingEvent } from '../generated/templates/TokenManag
 import { RevokeVesting as RevokeVestingEvent } from '../generated/templates/TokenManager/TokenManager'
 import { ScriptResult as ScriptResultEvent } from '../generated/templates/TokenManager/TokenManager'
 import { RecoverToVault as RecoverToVaultEvent } from '../generated/templates/TokenManager/TokenManager'
+import { MiniMeToken as MiniMeTokenTemplate } from '../generated/templates'
+import { InitializeCall } from '../generated/templates/TokenManager/TokenManager'
+import { TokenManager as TokenManagerContract } from '../generated/templates/TokenManager/TokenManager'
+
+export function handleProxyInitialize(call: InitializeCall): void {
+  let tokenManagerAddress = call.to
+
+  let tokenManagerContract = TokenManagerContract.bind(tokenManagerAddress)
+  let tokenAddress = tokenManagerContract.token()
+
+  MiniMeTokenTemplate.create(tokenAddress)
+}
 
 export function handleNewVesting(event: NewVestingEvent): void {}
 export function handleRevokeVesting(event: RevokeVestingEvent): void {}
