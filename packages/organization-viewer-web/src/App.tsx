28a867690d175925dcca3c168f64e2e7524b6f54
Pierre Bertet <hello@pierre.world>
2020-05-27 18:51:55 +0100

    Remove unused imports

--- packages/organization-viewer-web/src/App.tsx
+++ packages/organization-viewer-web/src/App.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { useEffect, useState } from 'react'
 import { css, jsx } from '@emotion/core'
-import { connect, Organization } from 'plumbery-core'
+import { connect } from 'plumbery-core'
 import Main from './Main'
 import OrgApps from './OrgApps'
 import OrgInfo from './OrgInfo'
