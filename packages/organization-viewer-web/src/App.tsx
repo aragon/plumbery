f86144ca31cf91bd5e3c1b4cbc7c584bbc40e01a
Pierre Bertet <hello@pierre.world>
2020-05-27 18:52:28 +0100

    Org viewer web: add DAO examples for multiple chains

--- packages/organization-viewer-web/src/App.tsx
+++ packages/organization-viewer-web/src/App.tsx
@@ -9,14 +9,23 @@ import OrgPermissions from './OrgPermissions'
 import TextButton from './TextButton'
 import { useCancellableAsync } from './generic-hooks'
 
-const ORG_ADDRESSES = new Map([
+const ORG_ADDRESSES_MAINNET_STAGING = new Map([
+  ['piedao', '0x0c188b183ff758500d1d18b432313d10e9f6b8a4'],
+])
+
+const ORG_ADDRESSES_MAINNET = new Map([
   ['a1', '0x635193983512c621E6a3E15ee1dbF36f0C0Db8E0'],
+])
+
+const ORG_ADDRESSES_RINKEBY = new Map([
   ['org1', '0x0146414e5a819240963450332f647dfb7c722af4'],
   ['org2', '0x00018d22ece8b2ea4e9317b93f7dff67385693d8'],
-  // ['td.aragonid.eth', '0xa9Aad8e278eECf369c42F78D5A3f2d866DE902C8'],
-  // ['hive.aragonid.eth', '0xe520428C232F6Da6f694b121181f907931fD2211'],
-  // ['mesh.aragonid.eth', '0xa48300a4E89b59A79452Db7d3CD408Df57f4aa78'],
+  ['td.aragonid.eth', '0xa9Aad8e278eECf369c42F78D5A3f2d866DE902C8'],
+  ['hive.aragonid.eth', '0xe520428C232F6Da6f694b121181f907931fD2211'],
+  ['mesh.aragonid.eth', '0xa48300a4E89b59A79452Db7d3CD408Df57f4aa78'],
 ])
+
+const ORG_ADDRESSES = ORG_ADDRESSES_MAINNET_STAGING
 
 function addressFromOrgName(orgName: string) {
   return (
