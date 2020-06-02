37848b818fead898920ce334ceb2b70fc6d94bc4
Pierre Bertet <hello@pierre.world>
2020-06-02 02:20:46 +0100

    Remove aragon/connect packages

--- packages/connector-thegraph/src/types.ts
+++
@@ -1,6 +0,0 @@
-import { OperationResult } from "@urql/core"
-
-export type QueryResult = OperationResult<any>
-export type DataGql = any
-
-export type ParseFunction = (data: DataGql) => {}
--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-export function useAragon() {}
-
-export function AragonConnector({ connector, signer }) {}
